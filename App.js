0bc90b6ca48a5a6cf6783d86da6c4821f719e905
jmbul <jerald.mark.buljatin@gmail.com>
2026-03-13 22:36:06 +0800

    Added Button Handlers and Styles

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
-import { useRef } from "react";
+import {StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {useRef, useState} from "react";
 import { debounce } from "lodash";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [goals, setGoals] = useState([]);
+  const inputRef = useRef(null);
+  const [isDisabled, setIsDisabled] = useState(true);
 
   const handler = useRef(
       debounce((text) => {
         console.log("Debounced:", text);
+        setEnteredGoalText(text)
       }, 500)
   ).current;
 
   function goalInputHandler(enteredText) {
     handler(enteredText);
+    if (enteredText) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
   }
 
-  function addNewGoalHandler() {}
+  function addNewGoalHandler() {
+    setGoals((currentGoals) => [...currentGoals, enteredGoalText]);
+    console.log("Adding new goal :)) - ", goals.join(', '));
+    if (inputRef.current) {
+      inputRef.current.clear();   // clear TextInput
+      setIsDisabled(true);
+    }
+  }
 
   return (
     <View style={styles.appContainer}>
@@ -23,11 +40,21 @@ export default function App() {
             placeholder='Your Current Goal!'
             onChangeText={goalInputHandler}
             style={styles.textInput}
+            onSubmitEditing={addNewGoalHandler}
+            ref={inputRef}
         />
-        <Button title='Add Goal'></Button>
+        <TouchableOpacity
+            onPress={addNewGoalHandler}
+            disabled={isDisabled}
+            style={isDisabled ? styles.buttonDisabled : styles.buttonEnabled}
+        >
+          <Text style={isDisabled ? styles.buttonTextDisabled : styles.buttonText}>
+            Add Goal
+          </Text>
+        </TouchableOpacity>
       </View>
       <View style={styles.goalsWrapper}>
-        <Text>List of goals :) ...</Text>
+        {goals.map((goal, index) => (<Text>{goal}</Text>))}
       </View>
     </View>
   );
@@ -56,5 +83,31 @@ const styles = StyleSheet.create({
   },
   goalsWrapper: {
     flex: 5
-  }
+  },
+  buttonEnabled: {
+    backgroundColor: "blue",
+    height: 40,                 // match TextInput height
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+    flex: 2
+  },
+  buttonDisabled: {
+    backgroundColor: "#fff",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    height: 40,                 // match TextInput height
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+    flex: 2
+  },
+  buttonText: {
+    fontWeight: "bold",
+    color: "white",              // for enabled
+  },
+  buttonTextDisabled: {
+    fontWeight: "bold",
+    color: "#4d4d4d",           // for disabled
+  },
 })
