a5546133c9daaac07d0979536a36a580f532af26
jmbul <jerald.mark.buljatin@gmail.com>
2026-03-13 21:48:42 +0800

    Added Debouncer

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import { useRef } from "react";
+import { debounce } from "lodash";
 
 export default function App() {
+
+  const handler = useRef(
+      debounce((text) => {
+        console.log("Debounced:", text);
+      }, 500)
+  ).current;
+
+  function goalInputHandler(enteredText) {
+    handler(enteredText);
+  }
+
+  function addNewGoalHandler() {}
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+            placeholder='Your Current Goal!'
+            onChangeText={goalInputHandler}
+            style={styles.textInput}
+        />
+        <Button title='Add Goal'></Button>
+      </View>
+      <View style={styles.goalsWrapper}>
+        <Text>List of goals :) ...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
-});
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+  },
+  goalsWrapper: {
+    flex: 5
+  }
+})
